b7b143a4173ad7c9afbd5fde92cfd08a8e37a93e
Sebastian <sebnun@gmail.com>
2016-03-27 01:08:51 -0300

    Implemented NSUndoManager functionality

--- RaiseMan/Document.swift
+++ RaiseMan/Document.swift
@@ -8,9 +8,26 @@
 
 import Cocoa
 
-class Document: NSDocument {
+private var KVOContext: Int = 0
+
+class Document: NSDocument, NSWindowDelegate {
     
-    var employees: [Employee] = []
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var arrayController: NSArrayController!
+    
+    var employees: [Employee] = [] {
+        willSet {
+            for employee in employees {
+                stopObservingEmployee(employee)
+            }
+        }
+        didSet {
+            for employee in employees {
+                startObservingEmployee(employee)
+            }
+        }
+    }
+    
 
     override init() {
         super.init()
@@ -54,6 +71,101 @@ class Document: NSDocument {
             return true
         }
     }
+    
+    //MARK: - Actions
+    
+    @IBAction func addEmployee(sender: NSButton) {
+        
+        let windowController = windowControllers[0]
+        let window = windowController.window!
+        
+        let endedEditing = window.makeFirstResponder(window)
+        
+        if !endedEditing {
+            return
+        }
+        
+        let undo: NSUndoManager = undoManager!
+        
+        if undo.groupingLevel > 0 {
+            undo.endUndoGrouping()
+            undo.beginUndoGrouping()
+        }
+        
+        
+        
+        let employee = arrayController.newObject() as! Employee
+        arrayController.addObject(employee)
+        arrayController.rearrangeObjects()
+        
+        let sortedEmployees = arrayController.arrangedObjects as! [Employee]
+        
+        let row = sortedEmployees.indexOf(employee)!
+        
+        tableView.editColumn(0, row: row, withEvent: nil, select: true)
+    }
+    
+    //MARK: - Accessors
+    
+    func insertObject(employee: Employee, inEmployeesAtIndex index: Int) {
+        
+        let undo: NSUndoManager = undoManager!
+        undo.prepareWithInvocationTarget(self).removeObjectFromEmployeesAtIndex(employees.count)
+        
+        if !undo.undoing {
+            undo.setActionName("Add Person")
+        }
+        
+        employees.append(employee)
+    }
 
+    func removeObjectFromEmployeesAtIndex(index: Int) {
+        
+        let employee: Employee = employees[index]
+        
+        let undo: NSUndoManager = undoManager!
+        undo.prepareWithInvocationTarget(self).insertObject(employee, inEmployeesAtIndex: index)
+        
+        if !undo.undoing {
+            undo.setActionName("Remove Person")
+        }
+        
+        employees.removeAtIndex(index)
+    }
+    
+    //MARK: - Key Value Observing
+    
+    func startObservingEmployee(employee: Employee) {
+        employee.addObserver(self, forKeyPath: "name", options: .Old, context: &KVOContext)
+        employee.addObserver(self, forKeyPath: "raise", options: .Old, context: &KVOContext)
+        
+    }
+    
+    func stopObservingEmployee(employee: Employee) {
+        employee.removeObserver(self, forKeyPath: "name", context: &KVOContext)
+        employee.removeObserver(self, forKeyPath: "raise", context: &KVOContext)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        
+        if context != &KVOContext {
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+            return
+        }
+        
+        var oldValue: AnyObject? = change![NSKeyValueChangeOldKey]
+        if oldValue is NSNull {
+            oldValue = nil
+        }
+        
+        let undo: NSUndoManager  = undoManager!
+        undo.prepareWithInvocationTarget(object!).setValue(oldValue, forKeyPath: keyPath!)
+    }
+    
+    //MARK: - NSWindowDelegate
+    
+    func windowWillClose(notification: NSNotification) {
+        employees = []
+    }
 }
 
