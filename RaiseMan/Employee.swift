e617b40d292e2a3bc9f57681d9264b68673fb60f
Sebastian <sebnun@gmail.com>
2016-03-28 19:38:40 -0300

    Implemented archiving and document saving capabilities

--- RaiseMan/Employee.swift
+++ RaiseMan/Employee.swift
@@ -8,7 +8,27 @@
 
 import Foundation
 
-class Employee: NSObject {
+class Employee: NSObject, NSCoding {
     var name: String? = "New Employee"
     var raise: Float = 0.05
+    
+    //MARK: - NSCoding
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObjectForKey("name") as! String?
+        raise = aDecoder.decodeFloatForKey("raise")
+        super.init()
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        if let name = name {
+            aCoder.encodeObject(name, forKey: "name")
+        }
+        
+        aCoder.encodeFloat(raise, forKey: "raise")
+    }
 }
