455a8efd08d8b11b6e2df1cdc8faa2d5406d71a5
Sebastian <sebnun@gmail.com>
2016-05-15 21:48:15 -0300

    implemented French localization

--- RaiseMan/Document.swift
+++ RaiseMan/Document.swift
@@ -8,13 +8,13 @@
 
 import Cocoa
 
+import Cocoa
+
 private var KVOContext: Int = 0
 
 class Document: NSDocument, NSWindowDelegate {
-    
     @IBOutlet weak var tableView: NSTableView!
     @IBOutlet weak var arrayController: NSArrayController!
-    
     var employees: [Employee] = [] {
         willSet {
             for employee in employees {
@@ -28,137 +28,107 @@ class Document: NSDocument, NSWindowDelegate {
         }
     }
     
-
-    override init() {
-        super.init()
-        // Add your subclass-specific initialization here.
-    }
-
-    override class func autosavesInPlace() -> Bool {
-        return true
-    }
-
-    override var windowNibName: String? {
-        return "Document"
-    }
-
-    override func dataOfType(typeName: String) throws -> NSData {
-        
-        tableView.window?.endEditingFor(nil)
-        
-        return NSKeyedArchiver.archivedDataWithRootObject(employees)
-    }
-
-    override func readFromData(data: NSData, ofType typeName: String) throws {
-        
-        employees = NSKeyedUnarchiver.unarchiveObjectWithData(data) as! [Employee]
-    }
-
-    func validateRaise(raiseNumberPointer: AutoreleasingUnsafeMutablePointer<NSNumber?>, error outError: NSErrorPointer) -> Bool {
-        let raiseNumber = raiseNumberPointer.memory
-        
-        if raiseNumber == nil {
-            let domain = "UserInputValidationErrorDomain"
-            let code = 0
-            let userInfo = [NSLocalizedDescriptionKey: "An employee's raise must be a number"]
-            outError.memory = NSError(domain: domain, code: code, userInfo: userInfo)
-            
-            return false
-        } else {
-            return true
-        }
-    }
-    
-    //MARK: - Actions
+    // MARK: - Actions
     
     @IBAction func addEmployee(sender: NSButton) {
-        
         let windowController = windowControllers[0]
         let window = windowController.window!
         
         let endedEditing = window.makeFirstResponder(window)
-        
         if !endedEditing {
+            print("Unable to end editing.")
             return
         }
         
         let undo: NSUndoManager = undoManager!
         
+        // Has an edit occurred already in this event?
         if undo.groupingLevel > 0 {
+            // Close the last group
             undo.endUndoGrouping()
+            // Open a new group
             undo.beginUndoGrouping()
         }
         
-        
-        
+        // Create the object
         let employee = arrayController.newObject() as! Employee
+        
+        // Add it to the array controller's content array
         arrayController.addObject(employee)
+        
+        // Re-sort (in case the user has sorted a column)
         arrayController.rearrangeObjects()
         
+        // Get the sorted array
         let sortedEmployees = arrayController.arrangedObjects as! [Employee]
         
+        // Find the object just added
         let row = sortedEmployees.indexOf(employee)!
         
+        // Begin the edit in the first column
+        print("starting edit of \(employee) in row \(row)")
         tableView.editColumn(0, row: row, withEvent: nil, select: true)
     }
     
     @IBAction func removeEmployees(sender: NSButton) {
-        
         let selectedPeople: [Employee] = arrayController.selectedObjects as! [Employee]
-        
         let alert = NSAlert()
-        alert.messageText = "Do you really want to remove these people?"
-        alert.informativeText = "\(selectedPeople.count) people will be removed"
-        alert.addButtonWithTitle("Remove")
-        alert.addButtonWithTitle("Cancel")
-        
+        alert.messageText = NSLocalizedString("REMOVE_MESSAGE", comment: "The remove alert's messageText")
+        let informativeFormat = NSLocalizedString("REMOVE_INFORMATIVE %d", comment: "The remove alert's informativeText")
+        alert.informativeText = String(format: informativeFormat, selectedPeople.count)
+        let removeButtonTitle = NSLocalizedString("REMOVE_DO", comment: "The alert's informative text")
+        alert.addButtonWithTitle(removeButtonTitle)
+        let removeCancelTitle = NSLocalizedString("REMOVE_CANCEL", comment: "The remove alert's cancel button")
+        alert.addButtonWithTitle(removeCancelTitle)
         let window = sender.window!
-        
-        alert.beginSheetModalForWindow(window) { (response) in
-            
+        alert.beginSheetModalForWindow(window, completionHandler: { (response) -> Void in
+            // If the user chose "Remove", tell the array controller to delete the people
             switch response {
             case NSAlertFirstButtonReturn:
+                // The array controller will delete the selected objects
+                // The argument to remove() is ignored
                 self.arrayController.remove(nil)
-            default:
-                break
+            default: break
             }
-        }
+        })
     }
     
-    //MARK: - Accessors
+    // MARK: - Accessors
     
     func insertObject(employee: Employee, inEmployeesAtIndex index: Int) {
+        print("adding \(employee) to the employees array")
         
+        // Add the inverse of this operation to the undo stack
         let undo: NSUndoManager = undoManager!
         undo.prepareWithInvocationTarget(self).removeObjectFromEmployeesAtIndex(employees.count)
-        
         if !undo.undoing {
             undo.setActionName("Add Person")
         }
         
         employees.append(employee)
     }
-
+    
     func removeObjectFromEmployeesAtIndex(index: Int) {
-        
         let employee: Employee = employees[index]
         
+        print("removing \(employee) from the employees array")
+        
+        // Add the inverse of this operation to the undo stack
         let undo: NSUndoManager = undoManager!
         undo.prepareWithInvocationTarget(self).insertObject(employee, inEmployeesAtIndex: index)
-        
         if !undo.undoing {
             undo.setActionName("Remove Person")
         }
         
+        // Remove the employee from the array
         employees.removeAtIndex(index)
     }
     
-    //MARK: - Key Value Observing
+    // MARK: - Key Value Observing
     
     func startObservingEmployee(employee: Employee) {
         employee.addObserver(self, forKeyPath: "name", options: .Old, context: &KVOContext)
         employee.addObserver(self, forKeyPath: "raise", options: .Old, context: &KVOContext)
-        
     }
     
     func stopObservingEmployee(employee: Employee) {
@@ -167,25 +137,71 @@ class Document: NSDocument, NSWindowDelegate {
     }
     
     override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
-        
-        if context != &KVOContext {
-            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+        guard context == &KVOContext else {
+            // If the context does not match, this message
+            //   must be intended for our superclass.
+            super.observeValueForKeyPath(keyPath,
+                                         ofObject: object,
+                                         change: change,
+                                         context: context)
             return
         }
         
-        var oldValue: AnyObject? = change![NSKeyValueChangeOldKey]
-        if oldValue is NSNull {
-            oldValue = nil
+        if let keyPath = keyPath, object = object, change = change {
+            var oldValue: AnyObject? = change[NSKeyValueChangeOldKey]
+            if oldValue is NSNull {
+                oldValue = nil
+            }
+            
+            let undo: NSUndoManager = undoManager!
+            print("oldValue=\(oldValue)")
+            undo.prepareWithInvocationTarget(object).setValue(oldValue,
+                                                              forKeyPath: keyPath)
         }
-        
-        let undo: NSUndoManager  = undoManager!
-        undo.prepareWithInvocationTarget(object!).setValue(oldValue, forKeyPath: keyPath!)
     }
     
-    //MARK: - NSWindowDelegate
+    // MARK - Lifecycle
+    
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+        
+    }
+    
+    // MARK - NSDocument Overrides
+    
+    override func windowControllerDidLoadNib(aController: NSWindowController) {
+        super.windowControllerDidLoadNib(aController)
+        
+        // Add any code here that needs to be executed once the windowController has loaded the document's window.
+    }
+    
+    override class func autosavesInPlace() -> Bool {
+        return true
+    }
+    
+    override var windowNibName: String {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return "Document"
+    }
+    
+    override func dataOfType(typeName: String) throws -> NSData {
+        // End editting
+        tableView.window!.endEditingFor(nil)
+        
+        // Create an NSData object from the employees array
+        return NSKeyedArchiver.archivedDataWithRootObject(employees)
+    }
+    
+    override func readFromData(data: NSData, ofType typeName: String) throws {
+        print("About to read data of type \(typeName).");
+        employees = NSKeyedUnarchiver.unarchiveObjectWithData(data) as! [Employee]
+    }
+    
+    // MARK: - NSWindowDelegate
     
     func windowWillClose(notification: NSNotification) {
         employees = []
     }
 }
-
